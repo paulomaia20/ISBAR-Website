6b4c7a5273dd4a7088d88d2d600f6cf39d0478ee
paulomaia20 <up201406159@fe.up.pt>
2018-11-21 21:54:09 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+$('#btn-next').on('click',function(event)
+
+{    console.log('aqui');
+    event.preventDefault();
+
+  // postBodyElement = event.target.parentNode.parentNode.childNodes[1];
+  //  var postBody=event.target.parentNode.parentNode.childNodes[1].textContent;
+ //   postid=event.target.parentNode.parentNode.dataset['postid'];
+  
+}
+);
